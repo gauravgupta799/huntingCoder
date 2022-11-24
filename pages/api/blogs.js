d07643e70e82ac78d588d55aa17277fd617856e6
gauravgupta <gaurav.mobilefirst@gmail.com>
2022-11-24 18:18:24 +0530

    changes

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,11 +2,13 @@ import * as fs from 'fs';
 // http://localhost:3000/api/blogs
 
 export default async function handler(req, res) {
-   const data = await fs.promises.readdir('BlogsData');
+   let data = await fs.promises.readdir('BlogsData');
+   // data = data.slice(0, parseInt(req.query.count))
+   let myfile;
    let allBlogs= [];
    for(let index=0; index < data.length; index++){
       const item = data[index];
-      let myfile = await fs.promises.readFile(`BlogsData/${item}`,'utf-8')
+      myfile = await fs.promises.readFile(`BlogsData/${item}`,'utf-8')
       allBlogs.push(JSON.parse(myfile));
    }
    res.status(200).json(allBlogs);
